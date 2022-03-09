7f024b0054f5e8a1933e191db2e9e435f8e53517
Nicole Candiotti <nccandiotti@gmail.com>
2022-03-09 10:26:54 -0500

    all fetches working

--- src/components/MyBooksPage.js
+++ src/components/MyBooksPage.js
@@ -1,6 +1,14 @@
-import React from "react"
+import { useEffect } from "react"
+
+// need to figure out how to interpolate id based on current user
+// const id =
 
 function MyBooksPage() {
+  useEffect(() => {
+    fetch(`http://localhost:9292/mybooks/42`)
+      .then((res) => res.json())
+      .then((r) => console.log(r))
+  }, [])
   return <div>Hello from My Books Page</div>
 }
 
